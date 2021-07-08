4d0173511776e5f2595051bed7d06b05decf2640
ayushiitbhu2800 <71924239+ayushiitbhu2800@users.noreply.github.com>
2021-07-08 09:33:56 +0530

    Update About.js

--- MyComponents/About.js
+++ MyComponents/About.js
@@ -3,8 +3,7 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+            <p>This is a simple To Do List Web Application made by Ayush Gupta</p>
         </div>
     )
 }
